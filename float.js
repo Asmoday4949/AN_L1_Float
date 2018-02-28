8ee323112bbf61a47a68c8d1991f9929d9683d62
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-02-28 20:56:28 +0100

    create empty object for a float

--- float.js
+++ float.js
@@ -2,3 +2,18 @@
 Authors : Biloni Kim, Fleury Malik, Bulloni Lucas
 Description : float class
 */
+function Float(num)
+{
+  this.sign = undefined;
+  this.exponent = [];
+  this.fraction = [];
+
+  this.build();
+}
+
+Float.prototype = {
+  /*calculate the float table of bits*/
+  this.build: function() {
+
+  }
+}
