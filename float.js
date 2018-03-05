77e9d969ec86d5772e4f387905e801c5c2645717
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-03-05 10:25:18 +0100

    add code to get the number of bits and creating a new float

--- float.js
+++ float.js
@@ -2,7 +2,7 @@
 Authors : Biloni Kim, Fleury Malik, Bulloni Lucas
 Description : float class
 */
-function Float(num)
+function Float(num, bits)
 {
   this.sign = undefined;
   this.exponent = [];
@@ -13,7 +13,27 @@ function Float(num)
 
 Float.prototype = {
   /*calculate the float table of bits*/
-  this.build: function() {
+  this.build: function()
+  {
 
   }
+
+  this.add: function(float)
+  {
+
+  }
+
+  this.soustract: function(float)
+  {
+
+  }
+}
+
+function processFloat()
+{
+  let inputVal = document.getElementById("float");
+  let nbBits = parseInt(document.getElementById("nbBits"));
+
+  let float = new Float(inputVal, nbBits);
+
 }
